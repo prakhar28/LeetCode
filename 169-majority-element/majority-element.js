39bfe961f56f5ce4af610be9c4f526dc8b3234e2
Prakhar Jain <prakharjn28@gmail.com>
2024-10-18 23:25:36 -0700

    Time: 15 ms (100.00%) | Memory: 53.5 MB (17.89%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,15 +3,8 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const map = new Map()
-    const len = nums.length
-    for(let i of nums) {
-        if(map.has(i)){
-            map.set(i, map.get(i)+1)
-            
-        } else {
-            map.set(i, 1)
-        }
-        if(map.get(i) > (len/2)) return i
-    }
+    const len = Math.floor(((nums.length)/2))
+    nums.sort()
+    console.log("nums", nums, len)
+    return nums[len]
 };
