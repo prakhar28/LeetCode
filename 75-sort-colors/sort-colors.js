c473a429aa3c2848fefaf56a172987b1fe42883c
Prakhar Jain <prakharjn28@gmail.com>
2024-10-19 12:59:41 -0700

    Time: 2 ms (98.46%) | Memory: 49.2 MB (45.34%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let swapped;
+    do {
+        swapped = false
+        for(let i = 0; i< nums.length -1; i++) {
+            if(nums[i] > nums[i+1]) {
+               let temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+                swapped = true
+            }
+        }
+    } while (swapped)
+};
