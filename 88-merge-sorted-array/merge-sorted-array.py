b42aec28753dee18014b2fd56ac8f6735b5e4e25
Prakhar Jain <prakharjn28@gmail.com>
2026-07-18 01:21:53 -0400

    sol:  - Runtime: 0 ms (100.00%), Space: 19.2 MB (95.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,7 +7,7 @@ class Solution:
         p2 = n - 1
         p = m + n -1
 
-        while p1 > 0 and p2 > 0:
+        while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
