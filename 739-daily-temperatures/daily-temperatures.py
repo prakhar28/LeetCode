413262a310ed8057e2c61c7053f03e678225afc4
Prakhar Jain <prakharjn28@gmail.com>
2026-07-23 09:26:16 -0400

    sol:  - Runtime: 0 ms (N/A), Space: 19 MB (N/A) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -6,9 +6,7 @@ class Solution:
 
         for i in range(len(temperatures)):
             while currStack and temperatures[i] > temperatures[currStack[-1]]:
-                print("curSta, i", currStack, i)
                 popped_index = currStack.pop()
-                print("popi", popped_index)
                 daysArr[popped_index] = i - popped_index
             currStack.append(i)
 
