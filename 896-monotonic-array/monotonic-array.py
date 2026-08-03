d7f6df6e5c349c487f8c26d128c2c31ed60b39c7
Prakhar Jain <prakharjn28@gmail.com>
2026-08-03 00:33:36 -0400

    sol:  - Runtime: 0 ms (N/A), Space: 19.2 MB (N/A) - LeetSync

--- 896-monotonic-array/monotonic-array.py
+++ 896-monotonic-array/monotonic-array.py
@@ -4,9 +4,9 @@ class Solution:
         isDec = True
 
         for i in range(1, len(nums)):
-            if nums[i] >= nums[i-1]:
+            if nums[i] > nums[i-1]:
                 isDec = False
-            if nums[i] <= nums[i-1]:
+            if nums[i] < nums[i-1]:
                 isInc = False
         
         return isInc or isDec
