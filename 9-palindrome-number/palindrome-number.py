3da761a4af65135d237ab64426c307f6d9f2360a
Prakhar Jain <prakharjn28@gmail.com>
2026-08-02 22:58:26 -0400

    sol:  - Runtime: 0 ms (100.00%), Space: 19 MB (97.76%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
-        print(x[0])
+        l = 0
+        r = len(x) - 1
+
+        while l < r:
+            if x[l] == x[r]:
+                l += 1
+                r -= 1
+            else:
+                return False
+        
+        return True
 
 
 
