8a00e0aedb90cf3edebc2aa1347d47c87137bf05
Prakhar Jain <prakharjn28@gmail.com>
2026-08-02 23:12:33 -0400

    sol:  - Runtime: 0 ms (N/A), Space: 19.3 MB (N/A) - LeetSync

---
+++ 2169-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        numberOfOperation = 0
+
+        while num1 != 0 and num2!= 0:
+            if num1 >= num2:
+                num1 = num1 - num2
+            else:
+                num2 = num2 - num1
+            numberOfOperation += 1
+        
+        return numberOfOperation
