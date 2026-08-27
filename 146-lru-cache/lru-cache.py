bfc7ec9457216405ef57299443cad7d887371cfc
Prakhar Jain <prakharjn28@gmail.com>
2026-08-27 19:54:06 -0400

    sol:  - Runtime: 0 ms (N/A), Space: 19.6 MB (N/A) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self,key=0,val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head, self.tail = Node(), Node()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        prev_node, next_node = node.prev, node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        
+    def insert(self, node):
+        prev_node = self.tail.prev
+
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.tail
+
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+
+        self.remove(node)
+        self.insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = val
+            self.remove(node)
+            self.insert(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+            self.insert(node)
+            if len(self.cache) > self.capacity:
+                lru = self.head.next
+                self.remove(lru)
+                del self.cache[lru.key]
+
+
+    
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
