806eaf2b224df273165eaabfb46454839947644f
Prakhar Jain <prakharjn28@gmail.com>
2026-08-31 14:27:38 -0400

    sol:  - Runtime: 0 ms (N/A), Space: 19.1 MB (N/A) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,16 @@
-from functools import cache
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
         if len(nums) == 1:
             return nums[0]
         
-        
+
         def house_line(houses):
-            @cache
-            def f(i):
-                if i >= len(houses):
-                    return 0
-                return max(houses[i] + f(i+2), f(i+1))
-            return f(0)
+            boxA, boxB = 0, 0
+            for i in range(len(houses) - 1, -1, -1):
+                ans = max(houses[i] + boxB, boxA)
+                boxB = boxA
+                boxA = ans
+                return boxA
         
         return max(house_line(nums[:-1]), house_line(nums[1:]))
